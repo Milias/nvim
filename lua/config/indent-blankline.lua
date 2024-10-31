22ef01875ebb6325eeb9f0f52963e133e76057a3
fgarciaf <francisco.garciaflorez@asml.com>
2024-10-31 11:56:24 +0100

    Muted indent line colors

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -12,13 +12,13 @@ local hooks = require "ibl.hooks"
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#B35D63" })
-  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#C4A76D" })
-  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#5590C7" })
-  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#AF8456" })
-  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#7EA365" })
-  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#A665B8" })
-  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#4899A3" })
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#8B4A4E" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#9E8759" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#456F99" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#8B6B45" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#657F50" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#825091" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#3A7A82" })
 end)
 
 require("ibl").setup { indent = { highlight = highlight } }
